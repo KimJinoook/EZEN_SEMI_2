ca09043bb3d42193d4ebb9f1d4d8b09e24a7c311
Runu09 <88878686+Runu09@users.noreply.github.com>
2022-05-24 18:10:53 +0900

    Add files via upload

--- semiProject2/src/main/java/com/semi2/util/Utility.java
+++ semiProject2/src/main/java/com/semi2/util/Utility.java
@@ -4,15 +4,17 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 public class Utility {
+	public static final String UPLOAD_PATH="pds_upload";
+	public static final String TEST_PATH="C:\\lecture\\jsp-ws\\semi2\\src\\main\\webapp\\review_upload";
 	
 	public static String displayRe(int step) {
 		String result="";
-		if(step>0){ //답변글이면..
+		if(step>0){ //�떟蹂�湲��씠硫�..
 			for(int j=0;j<step;j++){ 
-				result+="&nbsp;"; //공백먼저 붙이기
+				result+="&nbsp;"; //怨듬갚癒쇱� 遺숈씠湲�
 			}
 
-			result+="<img src='../img/board/re.gif'>"; // 공백+아이콘
+			result+="<img src='../img/board/re.gif'>"; // 怨듬갚+�븘�씠肄�
 		}
 
 		return result;
@@ -31,7 +33,7 @@ public class Utility {
 	public static String displayNew(Timestamp regdate) {
 		String result = "";
 		Date today = new Date();
-		// getTime() : 1970.01.01 이후 경과된 시간을 밀리초로 나타내는 메소드
+		// getTime() : 1970.01.01 �씠�썑 寃쎄낵�맂 �떆媛꾩쓣 諛�由ъ큹濡� �굹���궡�뒗 硫붿냼�뱶
 		long time = today.getTime() - regdate.getTime();
 		time = time / (1000 * 60 * 60);
 
